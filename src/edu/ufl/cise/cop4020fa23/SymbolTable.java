6b80bf8655bfffec9c053cf62ccb05a8d628f179
Thomas <93276519+tomgallego13@users.noreply.github.com>
2023-10-29 15:32:46 -0400

    Update SymbolTable.java

--- src/edu/ufl/cise/cop4020fa23/SymbolTable.java
+++ src/edu/ufl/cise/cop4020fa23/SymbolTable.java
@@ -3,9 +3,14 @@ package edu.ufl.cise.cop4020fa23;
 import edu.ufl.cise.cop4020fa23.ast.AST;
 import edu.ufl.cise.cop4020fa23.ast.NameDef;
 
+import java.util.HashMap;
+
 public class SymbolTable {
 
     //enterScope, leaveScope, insert(NameDef), lookup(name)
+
+    HashMap<String, NameDef> table = new HashMap<>();
+    // HashMap<String, Declaration> table = new HashMap<>();
 
     void enterScope() {}
 
