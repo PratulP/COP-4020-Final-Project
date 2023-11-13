c59d17064a4cb5640b014779f376130b8092f8ce
Thomas <93276519+tomgallego13@users.noreply.github.com>
2023-11-12 19:15:51 -0600

    Update CodeGenerator.java

--- src/edu/ufl/cise/cop4020fa23/CodeGenerator.java
+++ src/edu/ufl/cise/cop4020fa23/CodeGenerator.java
@@ -12,5 +12,124 @@ import edu.ufl.cise.cop4020fa23.exceptions.SyntaxException;
 
 public class CodeGenerator implements ASTVisitor {
 
+    String packageName;
+
+    public CodeGenerator(String packageName) {
+        this.packageName = packageName;
+    }
+
+
+    @Override
+    public Object visitProgram(Program program, Object arg) throws PLCCompilerException {
+        StringBuilder sb = new StringBuilder();
+        /*Type type = program.getType();
+        String sType = type.toString();
+
+        if (sType.equals("pixel") || sType.equals("PIXEL")) {
+            sType = "int";
+        }*/
+
+        sb.append("package ");
+        sb.append(packageName).append(";\n");
+
+        // append imports i think
+
+        sb.append("public class ").append(program.getName()).append("{\n");
+        sb.append(program.getType().toString().toLowerCase());
+        sb.append(" apply(");
+
+        for (int i = 0; i < program.getParams().size(); i++) {
+            program.getParams().get(i).visit(this, sb);
+            if (i != program.getParams().size() - 1) {
+                sb.append(", ");
+            }
+        }
+        sb.append("){\n");
+        
+
+
+        return null;
+    }
+
+    @Override
+    public Object visitBlock(Block block, Object arg) throws PLCCompilerException {
+        return null;
+    }
+
+    @Override
+    public Object visitNameDef(NameDef nameDef, Object arg) throws PLCCompilerException {
+        return null;
+    }
+
+    @Override
+    public Object visitDeclaration(Declaration declaration, Object arg) throws PLCCompilerException {
+        return null;
+    }
+
+    @Override
+    public Object visitConditionalExpr(ConditionalExpr conditionalExpr, Object arg) throws PLCCompilerException {
+        return null;
+    }
+
+    @Override
+    public Object visitBinaryExpr(BinaryExpr binaryExpr, Object arg) throws PLCCompilerException {
+        return null;
+    }
+
+    @Override
+    public Object visitConstExpr(ConstExpr constExpr, Object arg) throws PLCCompilerException {
+        return null;
+    }
+
+    @Override
+    public Object visitUnaryExpr(UnaryExpr unaryExpr, Object arg) throws PLCCompilerException {
+        return null;
+    }
+
+    @Override
+    public Object visitStringLitExpr(StringLitExpr stringLitExpr, Object arg) throws PLCCompilerException {
+        return null;
+    }
+
+    @Override
+    public Object visitNumLitExpr(NumLitExpr numLitExpr, Object arg) throws PLCCompilerException {
+        return null;
+    }
+
+    @Override
+    public Object visitIdentExpr(IdentExpr identExpr, Object arg) throws PLCCompilerException {
+        return null;
+    }
+
+
+    @Override
+    public Object visitBooleanLitExpr(BooleanLitExpr booleanLitExpr, Object arg) throws PLCCompilerException {
+        return null;
+    }
+
+    @Override
+    public Object visitWriteStatement(WriteStatement writeStatement, Object arg) throws PLCCompilerException {
+        return null;
+    }
+
+    @Override
+    public Object visitBlockStatement(StatementBlock statementBlock, Object arg) throws PLCCompilerException {
+        return null;
+    }
+
+    @Override
+    public Object visitReturnStatement(ReturnStatement returnStatement, Object arg) throws PLCCompilerException {
+        return null;
+    }
+
+    @Override
+    public Object visitAssignmentStatement(AssignmentStatement assignmentStatement, Object arg) throws PLCCompilerException {
+        return null;
+    }
+
+    @Override
+    public Object visitLValue(LValue lValue, Object arg) throws PLCCompilerException {
+        return null;
+    }
 
 }
