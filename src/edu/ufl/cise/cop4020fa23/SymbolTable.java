e5e27f920d814bb64b5caab8f2aaad5d041c97ae
PratulP <93286515+PratulP@users.noreply.github.com>
2023-11-27 16:16:47 -0500

    Update SymbolTable.java

--- src/edu/ufl/cise/cop4020fa23/SymbolTable.java
+++ src/edu/ufl/cise/cop4020fa23/SymbolTable.java
@@ -1,42 +1,94 @@
 package edu.ufl.cise.cop4020fa23;
 
-import edu.ufl.cise.cop4020fa23.ast.AST;
 import edu.ufl.cise.cop4020fa23.ast.NameDef;
-
+import edu.ufl.cise.cop4020fa23.exceptions.TypeCheckException;
 import java.util.HashMap;
 import java.util.Stack;
 
 public class SymbolTable {
+    private int currentScope;
+    private int nextScope;
+    private Stack<Integer> scopeStack;
+    private HashMap<String, Entry> symbolTable;
 
-    //enterScope, leaveScope, insert(NameDef), lookup(name)
+    public class Entry {
+        NameDef nameDef;
+        int scope;
+        Entry nextEntry;
 
-    HashMap<String, NameDef> table = new HashMap<>();
-    // HashMap<String, Declaration> table = new HashMap<>();
+        Entry(NameDef nameDef, int scope, Entry nextEntry) {
+            this.nameDef = nameDef;
+            this.scope = scope;
+            this.nextEntry = nextEntry;
+        }
 
-    Stack<HashMap<String, NameDef>> scope_stack;
-    //Stack<Integer> scope_stack;
-
-    int current_num;
-    int next_num;
-    void enterScope() {
-        //current_num = next_num++;
-        //scope_stack.push(current_num);
-        scope_stack.push(table);
-    }
-
-    void leaveScope() {
-        scope_stack.pop();
-        //current_num = scope_stack.pop();
-    }
-
-    /*void insert (NameDef namedef) {
-    }*/
-
-    void insert(String name, NameDef namedef) {
-        if (!scope_stack.isEmpty()) {
-            scope_stack.peek().put(name, namedef);
+        public int getScope() {
+            return scope;
         }
     }
 
-    void lookup(String name) {}
+    public SymbolTable() {
+        currentScope = 0;
+        nextScope = 1; 
+        scopeStack = new Stack<>();
+        symbolTable = new HashMap<>();
+        enterScope(); 
+    }
+
+    public void enterScope() {
+        currentScope = nextScope++;
+        scopeStack.push(currentScope);
+        System.out.println("Entering scope: " + currentScope);
+    }
+
+    public void leaveScope() {
+        if (!scopeStack.isEmpty()) {
+            System.out.println("Leaving scope: " + scopeStack.peek());
+            scopeStack.pop();
+            if (!scopeStack.isEmpty()) {
+                currentScope = scopeStack.peek();
+                System.out.println("Returning to scope: " + currentScope);
+            }
+        }
+    }
+
+
+    public boolean insert(NameDef nameDef) {
+        String ident = nameDef.getName();
+        Entry existingEntry = symbolTable.get(ident);
+
+        if (existingEntry != null && existingEntry.scope == currentScope) {
+            return false;
+        }
+
+        Entry newEntry = new Entry(nameDef, currentScope, existingEntry);
+        symbolTable.put(ident, newEntry);
+        return true;
+    }
+
+    public NameDef lookup(String ident) {
+        Entry entry = symbolTable.get(ident);
+        while (entry != null) {
+            if (scopeStack.contains(entry.scope)) {
+                return entry.nameDef;
+            }
+            entry = entry.nextEntry;
+        }
+        return null; 
+    }
+
+    public int getCurrentScope() {
+        return currentScope;
+    }
+
+    public boolean lookupLocal(String ident) {
+        Entry entry = symbolTable.get(ident);
+        while (entry != null) {
+            if (entry.scope == currentScope) {
+                return true; 
+            }
+            entry = entry.nextEntry;
+        }
+        return false; 
+    }
 }
